8265d09aca1889be56a4ff8986cc366852f2b04a
MayaAngels <mayaalmeidaalves2024@gmail.com>
2026-04-28 19:53:21 -0300

    Final path fix

--- app/client/content/social/page.tsx
+++ app/client/content/social/page.tsx
@@ -1,7 +1,7 @@
 ﻿// app/client/content/social/page.tsx
 'use client';
 import { useEffect, useState } from 'react';
-import { useClientAuth } from '../../../../../../../../../components/client/ClientAuthProvider';
+import { useClientAuth } from '../../../../components/client/ClientAuthProvider';
 import { listSocialPosts } from '../../../../../../../../../lib/client/api';
 import Link from 'next/link';
 
