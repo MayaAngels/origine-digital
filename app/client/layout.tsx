992d3a81805619f8eea8f0db11f6d8672c075309
MayaAngels <mayaalmeidaalves2024@gmail.com>
2026-04-28 18:45:21 -0300

    Fix @/ imports in app/client/layout.tsx

--- app/client/layout.tsx
+++ app/client/layout.tsx
@@ -1,5 +1,5 @@
 ﻿// app/client/layout.tsx
-import { ClientAuthProvider } from '@/components/client/ClientAuthProvider';
+import { ClientAuthProvider } from '../components/client/ClientAuthProvider';
 import '../globals.css';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
