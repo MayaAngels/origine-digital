6eed96258fae3fa6236697661dc231b2768a4684
MayaAngels <mayaalmeidaalves2024@gmail.com>
2026-04-28 22:34:40 -0300

    Final stubs everywhere

--- lib/stripe-client.ts
+++ lib/stripe-client.ts
@@ -1,3 +1,3 @@
-import { loadStripe } from '@stripe/stripe-js'
+const stripe = { checkout: { sessions: { create: async () => ({ url: '/checkout' }) } } };
 
 export const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
