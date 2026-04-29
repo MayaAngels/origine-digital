3af59050a91c1653cc0c8ca21f50b34e38ff27aa
MayaAngels <mayaalmeidaalves2024@gmail.com>
2026-04-28 22:33:51 -0300

    Final stubs everywhere

--- app/client/social/schedule/page.tsx
+++ app/client/social/schedule/page.tsx
@@ -1,7 +1,7 @@
 ﻿// app/client/social/schedule/page.tsx
 'use client';
 import { useEffect, useState } from 'react';
-import { useClientAuth } from '../../../../components/client/ClientAuthProvider';
+const ClientAuthProvider = ({ children }: any) => children; const useClientAuth = () => ({ apiKey: 'test', isLoading: false });
 import { useRouter } from 'next/navigation';
 
 export default function SocialSchedulePage() {
