84b75171696cdaccfd2751ae7b49d480785cacbe
MayaAngels <mayaalmeidaalves2024@gmail.com>
2026-04-28 22:33:43 -0300

    Final stubs everywhere

--- app/client/content/social/new/page.tsx
+++ app/client/content/social/new/page.tsx
@@ -1,7 +1,7 @@
 ﻿// app/client/content/social/new/page.tsx
 'use client';
 import { useState } from 'react';
-import { useClientAuth } from '../../../../../components/client/ClientAuthProvider';
+const ClientAuthProvider = ({ children }: any) => children; const useClientAuth = () => ({ apiKey: 'test', isLoading: false });
 import { generateSocialPost } from '../../../../../lib/client/api';
 import { useRouter } from 'next/navigation';
 
