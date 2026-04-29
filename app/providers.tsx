4320b8e66afc4b429295e9002ebe3b9b13c7f491
MayaAngels <mayaalmeidaalves2024@gmail.com>
2026-04-28 23:08:34 -0300

    Fix CartProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,17 +1,30 @@
-import { SyntheticAgent } from '../components/agent/SyntheticAgent'
-"use client";
+'use client';
 
-import { ReactNode } from "react";
-const useCart = () => ({ cart: [], addToCart: () => {}, total: 0 });
+import React from 'react';
 
-type ProvidersProps = {
-  children: ReactNode;
-};
+const CartContext = React.createContext({
+    cart: [] as any[],
+    addToCart: (item: any) => {},
+    removeFromCart: (id: string) => {},
+    clearCart: () => {},
+    total: 0,
+});
 
-export function Providers({ children }: ProvidersProps) {
-  return (
-    <CartProvider>
-      {children}
-    </CartProvider>
-  );
+export function CartProvider({ children }: { children: React.ReactNode }) {
+    const [cart, setCart] = React.useState<any[]>([]);
+    
+    const addToCart = (item: any) => setCart([...cart, item]);
+    const removeFromCart = (id: string) => setCart(cart.filter(i => i.id !== id));
+    const clearCart = () => setCart([]);
+    const total = cart.reduce((sum, i) => sum + (i.price || 0), 0);
+
+    return (
+        <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, total }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+export function useCart() {
+    return React.useContext(CartContext);
 }
