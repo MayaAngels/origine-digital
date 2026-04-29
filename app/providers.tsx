d97dd450448661943b8b3ace7e712efe2e8a45a2
MayaAngels <mayaalmeidaalves2024@gmail.com>
2026-04-29 01:05:21 -0300

    Revert to original providers without SessionProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,30 +1,9 @@
-"use client";
-
 import { CartProvider as CartProviderComponent } from "@/components/cart/CartProvider";
-import { SessionProvider } from "@/components/auth/SessionProvider";
-import { useEffect, useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  // Return without auth during SSR
-  if (!mounted) {
     return (
-      <CartProviderComponent>
-        {children}
-      </CartProviderComponent>
+        <CartProviderComponent>
+            {children}
+        </CartProviderComponent>
     );
-  }
-
-  return (
-    <SessionProvider>
-      <CartProviderComponent>
-        {children}
-      </CartProviderComponent>
-    </SessionProvider>
-  );
 }
