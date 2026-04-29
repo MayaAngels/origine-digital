7d82f0d234345700e583e5aead19cedd7437c8a2
MayaAngels <mayaalmeidaalves2024@gmail.com>
2026-04-28 21:12:29 -0300

    Remove @/ imports

--- components/products/ProductFilters.tsx
+++ components/products/ProductFilters.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { categories } from "@/lib/products/data";
+import { categories } from "../lib/products/data";
 
 interface ProductFiltersProps {
   selectedCategory: string;
