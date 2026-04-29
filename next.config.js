75699feb11b0b09e147e26078a08a2409d4e65e9
MayaAngels <mayaalmeidaalves2024@gmail.com>
2026-04-29 13:33:58 -0300

    Fix Vercel output directory

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'out',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
