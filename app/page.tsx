ab04af9210d47f43202cc0ef5ca4e90f7dc8495d
MayaAngels <mayaalmeidaalves2024@gmail.com>
2026-04-28 21:12:04 -0300

    Remove @/ imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-﻿import { HeroSection } from "@/components/marketing/HeroSection";
-import { FeaturedProducts } from "@/components/marketing/FeaturedProducts";
-import { FeaturedServices } from "@/components/marketing/FeaturedServices";
-import { CTASection } from "@/components/marketing/CTASection";
+﻿import { HeroSection } from "./components/marketing/HeroSection";
+import { FeaturedProducts } from "./components/marketing/FeaturedProducts";
+import { FeaturedServices } from "./components/marketing/FeaturedServices";
+import { CTASection } from "./components/marketing/CTASection";
 
 export default function HomePage() {
   return (
