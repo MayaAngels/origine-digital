b3768ccaec4e408ea429f19b058c184ccfa8ef30
MayaAngels <mayaalmeidaalves2024@gmail.com>
2026-04-28 21:12:28 -0300

    Remove @/ imports

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,8 +1,8 @@
 ﻿"use client";
 
 import Link from "next/link";
-import { useCart } from "@/contexts/CartContext";
-import { Product } from "@/lib/products/data";
+import { useCart } from "../contexts/CartContext";
+import { Product } from "../lib/products/data";
 
 interface ProductCardProps {
   product: Product;
