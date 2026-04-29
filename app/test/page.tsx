41034e97e670e70ba8385fbd7c9e901cb8d34b0d
MayaAngels <mayaalmeidaalves2024@gmail.com>
2026-04-29 01:33:41 -0300

    Create test page to isolate auth error

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Test Page - No Auth</h1>
+      <p>If this page loads, the error is somewhere else.</p>
+    </div>
+  );
+}
